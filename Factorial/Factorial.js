da0bbe048d337b86fd0513c958c40e76e91889ce
Chibgatullah Minhaz <chibgatullahminhaz@gmail.com>
2024-09-19 23:13:59 +0600

    chnages

--- Factorial/Factorial.js
+++ Factorial/Factorial.js
@@ -1,12 +1,12 @@
 let output = document.getElementById('fact');
 const btn = document.getElementById('check');
-let number;
+let  number;
 btn.addEventListener('click',()=>{
-     number =Number(document.getElementById('number').value);
+    number =Number(document.getElementById('number').value)
      console.log(number)
      function Factorial(n) {
         console.log(n)
-        if(n < 0) document.getElementById('Warnign').innerText = "Factorial Not Posible";
+        if(n < 0) return "Factorial Not Possible";
         if(n === 0 || n === 1) return 1;
         if (n % 2 === 0 ) {
             return  n * Factorial(n - 1);
