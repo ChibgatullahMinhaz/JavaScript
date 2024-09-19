39ac1865369a41969ec82ae8d4e7902a05eac35f
Chibgatullah Minhaz <chibgatullahminhaz@gmail.com>
2024-09-19 19:55:50 +0600

    vowel and consonent count

---
+++ vowelCount.js
@@ -0,0 +1,22 @@
+let str = "the quick brown fox jumps over the lazy dog";
+
+const vowel = "aeiouAEIOU";
+
+function vowelCount() {
+    let vcount = 0;
+    let cCount = 0;
+    for (let index = 0; index < str.length; index++) {
+        let charter = str[index].toLocaleLowerCase();
+        if (charter >= 'a' && charter <= 'z' && !vowel.includes(charter)) {
+           cCount++
+        }else if(charter >= 'a' && charter <= 'z' && vowel.includes(charter)){
+            vcount++
+        }else{
+           '🥰'
+            
+        }
+    }
+    return `Total Vowel : ${vcount} and Total Consonent : ${cCount}`;
+   
+}
+console.log(vowelCount())
