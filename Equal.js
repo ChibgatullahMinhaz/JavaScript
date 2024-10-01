2b8a151cd8872f0d3f1cb695ca40d8ba4149f111
Chibgatullah Minhaz <chibgatullahminhaz@gmail.com>
2024-10-02 00:29:06 +0600

    equal value check

---
+++ Equal.js
@@ -0,0 +1,17 @@
+let x = { name: "subscript" };
+let y = { name: "subscript" };
+
+if (x.name === y.name) {
+    console.log("equal")
+}else{
+    console.log("not Equal")
+}
+// object 
+let a = { name: "subscript" };
+let b = { name: "subscript" };
+
+if (a === b) {
+    console.log("equal")
+}else{
+    console.log("not Equal")
+}
